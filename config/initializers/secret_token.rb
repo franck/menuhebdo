9ebe480559eb10346f8e2e86d4f28d2618fb9095
Franck D'agostini <franck.dagostini@gmail.com>
2013-12-15 12:59:18 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Monmenuhebdo::Application.config.secret_key_base = '4817117e8d29fc9f91bdf3f262e53f8c2824f3d55fda2249de53556a70412ed818a0fdb082dc06b20e303396864476b9841c4b4bf8136bdc97ccb8aeec4bc618'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Monmenuhebdo::Application.config.session_store :cookie_store, key: '_monmenuhebdo_session'
