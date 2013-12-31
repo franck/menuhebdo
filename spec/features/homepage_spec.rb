61078e6451d7110b0a85ae45bf404ad3df186c34
Franck D'agostini <franck.dagostini@gmail.com>
2013-12-31 17:28:38 +0100

    redesign subscribers#new

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -8,7 +8,7 @@ feature "Homepage", %q{
 
   scenario "Homepage shows a teaser" do
     visit '/'
-    page.should have_content "Mon menu hebdo"
+    page.should have_content "Menu hebdo"
     page.should have_content "Recevez chaque semaine un menu pour la semaine"
   end
 
